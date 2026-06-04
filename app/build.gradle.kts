aef478959fe296af2f67830bc2ecc3d9069a4c4f
Graham <grimm.huidu@outlook.com>
2026-06-04 08:34:49 +0800

    release 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,25 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+    signingConfigs {
 
+        create("release") {
+
+            storeFile = file(
+                System.getenv("KEYSTORE_FILE")
+                    ?: "release.jks"
+            )
+
+            storePassword =
+                System.getenv("KEYSTORE_PASSWORD")
+
+            keyAlias =
+                System.getenv("KEY_ALIAS")
+
+            keyPassword =
+                System.getenv("KEY_PASSWORD")
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -25,6 +43,8 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig =
+                signingConfigs.getByName("release")
         }
     }
     compileOptions {
--- app/src/main/java/com/dhera/fitnessprogram/AppConfig.kt
+++ app/src/main/java/com/dhera/fitnessprogram/AppConfig.kt
@@ -2,6 +2,6 @@ package com.dhera.fitnessprogram
 
 object AppConfig {
     const val APP_NAME = "Fitness Program"
-    const val VERSION_NAME = "v1.0.2"
+    const val VERSION_NAME = "v1.0.3"
     const val CONTACT_EMAIL = "grimm.huidu@outlook.com"
 }
