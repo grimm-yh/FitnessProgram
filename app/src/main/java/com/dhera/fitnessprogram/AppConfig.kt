75cd970c6db6b29b4d4cf0e9cbb0deb2e244955c
Graham <grimm.huidu@outlook.com>
2026-06-07 20:07:40 +0800

    release 1.0.7-test

--- app/src/main/java/com/dhera/fitnessprogram/AppConfig.kt
+++ app/src/main/java/com/dhera/fitnessprogram/AppConfig.kt
@@ -2,6 +2,6 @@ package com.dhera.fitnessprogram
 
 object AppConfig {
     const val APP_NAME = "Fitness Program"
-    const val VERSION_NAME = "v1.0.7"
+    const val VERSION_NAME = "v1.0.7-test"
     const val CONTACT_EMAIL = "grimm.huidu@outlook.com"
 }
